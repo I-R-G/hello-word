18a677f48d206625f14e61d5fe8eef6314896a51
ianrg90 <ian.gladulich@gmail.com>
2021-07-07 16:07:35 -0300

    First attempt

---
+++ FreeCodeCamp-Final JS Challenges/roman-numeral.js
@@ -0,0 +1,20 @@
+function convertToRoman(num) {
+ 
+  const toObj = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: "CM", 1000: "M"
+  };
+
+  const numeral = Object.keys(toObj).map(x => parseInt(x)).reverse()
+
+  let roman = "";
+  for(let i = 0; i < numeral.length; i++){
+  
+    while (num >= numeral[i]){
+      roman += toObj[numeral[i]]
+      num -= numeral[i]
+    } 
+  }
+  return roman
+}
+
+const a = convertToRoman(36);
+console.log(a)

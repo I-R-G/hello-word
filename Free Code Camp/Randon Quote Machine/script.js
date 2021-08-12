9884b2f60ffff772aa5bba0b97f4cf5844826b32
ianrg90 <ian.gladulich@gmail.com>
2021-08-11 21:07:23 -0300

    Change to async JS

--- Free Code Camp/Randon Quote Machine/script.js
+++ Free Code Camp/Randon Quote Machine/script.js
@@ -1,20 +1,43 @@
 const button = document.querySelector("#new-quote")
 const quote = document.querySelector("#text")
 const author = document.querySelector("#author")
-const controls = document.querySelector(".buttons")
+const tweet = document.querySelector("#tweet-quote")
+const link = document.querySelector("a")
 const url = "https://api.quotable.io/random"
 
 
 
+let quoteText = ""
+let authorName = ""
+let quoteSize
 
-function getQuote(){
-    fetch(url).then(blob => blob.json())
-    .then(data => {
-        controls.style.transform = "translate(0px)"
-        quote.textContent = `"${data.content}"`
-        author.textContent = `-${data.author}`
-    })    
+async function getQuoteAndAuthor(){
+    const response = await fetch(url)
+    const data =  await response.json()
+
+    quoteText = data.content
+    authorName = data.author
+    quoteSize = data.length
+
+    quote.textContent = `${quoteText}`
+    author.textContent = `${authorName}`
 }
 
-button.addEventListener("click", getQuote)
+function tweetQuote(){
+    let tweetText = `${quoteText} \n\n "${authorName}"`
+    if(isNaN(quoteSize)){
+        alert('No quote to tweet. Click the "New quote" button to get a quote first !!!')
+       
+    }
+    else if (quoteSize > 140) {
+        alert('Ops, this quote is over 140 characters, too big to tweet !!!')
+       
+    }
+    else{
+        link.href = "https://twitter.com/intent/tweet?text=" + encodeURI(tweetText)
+        link.target = "_blank"
+    }
+}
 
+button.addEventListener("click", getQuoteAndAuthor)
+tweet.addEventListener("click", tweetQuote)
